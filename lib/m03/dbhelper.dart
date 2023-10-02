a1bd16c36fdb886212c38ae418c4986caebe1187
Frederic Davidsen <67452473+fdavidsen@users.noreply.github.com>
2023-10-02 14:25:07 +0700

    Update dbhelper.dart

--- lib/m03/dbhelper.dart
+++ lib/m03/dbhelper.dart
@@ -1,10 +1,11 @@
-import 'package:latihan_mobile_backend/m03/shopping_list.dart';
+import 'package:flutter_application_1/m03/shopping_list.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DBHelper {
   Database? _database;
   final String _table_name = 'shopping_list';
+  final String _table_history = 'shopping_list_history';
   final String _db_name = 'shoppinglist_database.db';
   final int _db_version = 1;
 
@@ -15,9 +16,11 @@ class DBHelper {
   Future<void> _openDB() async {
     _database ??= await openDatabase(
       join(await getDatabasesPath(), _db_name),
-      onCreate: (db, version) {
-        return db
-            .execute('CREATE TABLE $_table_name (id INTEGER PRIMARY KEY, name TEXT, sum INTEGER)');
+      onCreate: (db, version) async {
+        await db.execute(
+            'CREATE TABLE $_table_name (id INTEGER PRIMARY KEY, name TEXT, sum INTEGER)');
+        await db.execute(
+            'CREATE TABLE $_table_history (id INTEGER PRIMARY KEY, datetime DATETIME, name TEXT)');
       },
       version: _db_version,
     );
@@ -42,7 +45,7 @@ class DBHelper {
       where: 'id = ?',
       whereArgs: [id],
     );
-    print(DateTime.now());
+    await insertHistory();
   }
 
   Future<void> deleteAllShoppingList() async {
@@ -51,7 +54,8 @@ class DBHelper {
 
   Future<List<ShoppingList>> getShoppingList() async {
     if (_database != null) {
-      final List<Map<String, dynamic>> maps = await _database!.query(_table_name);
+      final List<Map<String, dynamic>> maps =
+          await _database!.query(_table_name);
       print(maps);
 
       return List.generate(
@@ -63,5 +67,13 @@ class DBHelper {
               ));
     }
     return [];
+  }
+
+  Future<void> insertHistory() async {
+    await _database?.insert(
+      _table_history,
+      {'datetime': DateTime.now()},
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
   }
 }
