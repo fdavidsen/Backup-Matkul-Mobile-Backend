26ef2057a2844f6e1b8babc4be597a517c76f8ad
Frederic Davidsen <67452473+fdavidsen@users.noreply.github.com>
2023-10-09 13:53:10 +0700

    Update http_helper.dart

--- lib/m04/http_helper.dart
+++ lib/m04/http_helper.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:http/http.dart' as http;
@@ -16,6 +17,20 @@ class HttpHelper {
     if (result.statusCode == HttpStatus.ok) {
       String responseBody = result.body;
       return responseBody;
+    }
+    return result.statusCode.toString();
+  }
+
+  Future<dynamic> getWeatherMedan() async {
+    var url = Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?q=medan&appid=903507f17d707fecd352d38301efba77');
+    http.Response result = await http.get(url);
+
+    if (result.statusCode == HttpStatus.ok) {
+      var responseBody = json.decode(result.body);
+      var temp = responseBody['main']['temp'] - 273;
+      print(temp);
+      return temp;
     }
     return result.statusCode.toString();
   }
