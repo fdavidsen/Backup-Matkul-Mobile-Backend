d57fd3734b085a3a2484308ded3e4b091988df40
Frederic Davidsen <67452473+fdavidsen@users.noreply.github.com>
2023-10-20 14:36:10 +0700

    Update home.dart

--- lib/m05/home.dart
+++ lib/m05/home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:latihan_mobile_backend/m05/detail_screen.dart';
-import 'package:latihan_mobile_backend/m05/http_helper.dart';
+import 'package:flutter_application_1/m05/detail_screen.dart';
+import 'package:flutter_application_1/m05/http_helper.dart';
 
 class Home5 extends StatefulWidget {
   const Home5({super.key});
@@ -14,16 +14,18 @@ class _Home5State extends State<Home5> {
   List? movies;
   List? filteredItems;
   final String iconBase = 'https://image.tmdb.org/t/p/w92/';
-  final String defaultImage = 'https://images.freeimages.com/images/large-previews/5eb/movie-clapboard-1184339.jpg';
+  final String defaultImage =
+      'https://images.freeimages.com/images/large-previews/5eb/movie-clapboard-1184339.jpg';
 
   Map<String, String> options = {
-    'Now Playing': 'now_playing',
-    'Popular': 'popular',
-    'Top Rated': 'top_rated',
-    'Upcoming': 'upcoming',
-    'Latest': 'latest',
+    'now_playing': 'Now Playing',
+    'popular': 'Popular',
+    'top_rated': 'Top Rated',
+    'upcoming': 'Upcoming',
+    'latest': 'Latest',
   };
   String selectedOption = 'now_playing';
+  String appBarTitle = 'Now Playing';
 
   final _controller = ScrollController();
 
@@ -53,8 +55,12 @@ class _Home5State extends State<Home5> {
     super.initState();
   }
 
+  final TextEditingController _searchController = TextEditingController();
+
   void filterSearchResults(String query) {
-    List searchResults = movies!.where((item) => item.title.toLowerCase().contains(query.toLowerCase())).toList();
+    List searchResults = movies!
+        .where((item) => item.title.toLowerCase().contains(query.toLowerCase()))
+        .toList();
     setState(() {
       filteredItems = searchResults;
     });
@@ -66,7 +72,7 @@ class _Home5State extends State<Home5> {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('My Movie'),
+        title: Text(appBarTitle),
       ),
       body: SingleChildScrollView(
         controller: _controller,
@@ -74,6 +80,7 @@ class _Home5State extends State<Home5> {
         child: Column(
           children: [
             TextField(
+              controller: _searchController,
               onChanged: filterSearchResults,
               decoration: const InputDecoration(
                 // labelText: "Search",
@@ -86,13 +93,15 @@ class _Home5State extends State<Home5> {
               icon: const Icon(Icons.keyboard_arrow_down),
               items: options.entries.map((entry) {
                 return DropdownMenuItem(
-                  value: entry.value,
-                  child: Text(entry.key),
+                  value: entry.key,
+                  child: Text(entry.value),
                 );
               }).toList(),
               onChanged: (String? newValue) {
                 setState(() {
+                  appBarTitle = options[newValue]!;
                   selectedOption = newValue!;
+                  _searchController.text = '';
                   loadData();
                 });
               },
@@ -100,28 +109,36 @@ class _Home5State extends State<Home5> {
             // Text(movies.toString()),
             ListView.builder(
                 shrinkWrap: true,
-                itemCount: filteredItems?.length == null ? 0 : filteredItems?.length,
+                itemCount:
+                    filteredItems?.length == null ? 0 : filteredItems?.length,
                 itemBuilder: (context, int position) {
-                  if (filteredItems![position].posterPath != null) {
-                    image = NetworkImage(iconBase + filteredItems![position].posterPath);
-                    print(image);
+                  if (filteredItems![position].posterPath != null &&
+                      filteredItems![position].posterPath != '') {
+                    image = NetworkImage(
+                        iconBase + filteredItems![position].posterPath);
                   } else {
                     image = NetworkImage(defaultImage);
                   }
+                  print(filteredItems![position].posterPath);
+                  print(image);
 
                   return Card(
                     color: Colors.white,
                     child: ListTile(
                       onTap: () {
-                        MaterialPageRoute route = MaterialPageRoute(builder: (_) => DetailScreen(movie: filteredItems![position]));
+                        MaterialPageRoute route = MaterialPageRoute(
+                            builder: (_) =>
+                                DetailScreen(movie: filteredItems![position]));
                         Navigator.push(context, route);
                       },
-                      // leading: CircleAvatar(
-                      //   backgroundImage: image,
-                      // ),
+                      leading: CircleAvatar(
+                        backgroundImage: image,
+                      ),
                       title: Text(filteredItems![position].title),
-                      subtitle:
-                          Text('Released: ' + filteredItems![position].releaseDate + ' - Vote: ' + filteredItems![position].voteAverage.toString()),
+                      subtitle: Text('Released: ' +
+                          filteredItems![position].releaseDate +
+                          ' - Vote: ' +
+                          filteredItems![position].voteAverage.toString()),
                     ),
                   );
                 }),
