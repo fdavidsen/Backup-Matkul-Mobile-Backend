1d9679b308190be0bbce0dcf7bbe0515dd6c82de
Frederic Davidsen <67452473+fdavidsen@users.noreply.github.com>
2023-12-04 14:27:17 +0700

    Update home.dart

--- lib/m11/home.dart
+++ lib/m11/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:location/location.dart';
+import 'package:localization/localization.dart';
 
 class Home11 extends StatefulWidget {
   const Home11({super.key});
@@ -9,41 +9,10 @@ class Home11 extends StatefulWidget {
 }
 
 class _Home11State extends State<Home11> {
-  var location;
-  Future _initLocationService() async {
-    location = Location();
-
-    if (!await location.serviceEnabled()) {
-      if (!await location.requestService()) {
-        return;
-      }
-    }
-
-    var permission = await location.hasPermission();
-    if (permission == PermissionStatus.denied) {
-      permission = await location.requestPermission();
-      if (permission != PermissionStatus.granted) {
-        return;
-      }
-    }
-
-    var loc = await location.getLocation();
-    print("${loc.latitude} ${loc.longitude}");
-  }
-
-  @override
-  void initState() {
-    _initLocationService();
-    super.initState();
-  }
+  bool _isChecked = false;
 
   @override
   Widget build(BuildContext context) {
-    location.onLocationChanged.listen((LocationData loc) {
-      print("${loc.latitude} ${loc.longitude}");
-    });
-    bool _isChecked = false;
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Semantic'),
@@ -57,11 +26,11 @@ class _Home11State extends State<Home11> {
               style: TextStyle(fontSize: 20),
             ),
           ),
-          const Padding(
-            padding: EdgeInsets.all(8.0),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
             child: TextField(
               decoration: InputDecoration(
-                  border: OutlineInputBorder(), labelText: 'Isikan Nama'),
+                  border: const OutlineInputBorder(), labelText: 'Name'.i18n()),
             ),
           ),
           Padding(
@@ -71,9 +40,9 @@ class _Home11State extends State<Home11> {
               children: [
                 ListTile(
                   leading: const Icon(Icons.help),
-                  title: const Text(
-                    'Hubungi',
-                    style: TextStyle(color: Colors.blue),
+                  title: Text(
+                    'Text_call'.i18n(),
+                    style: const TextStyle(color: Colors.blue),
                   ),
                   onTap: () {},
                 ),
@@ -100,7 +69,7 @@ class _Home11State extends State<Home11> {
                         _isChecked = value!;
                       });
                     }),
-                Text('Setuju')
+                Text('Checkbox-agree'.i18n())
               ],
             ),
           ),
@@ -111,12 +80,16 @@ class _Home11State extends State<Home11> {
               children: [
                 Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(onPressed: () {}, child: Text('Masuk')),
+                  child: ElevatedButton(
+                      onPressed: () {}, child: Text('Button-sign-out'.i18n())),
                 ),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child:
-                      ElevatedButton(onPressed: () {}, child: Text('Keluar')),
+                  child: ElevatedButton(
+                      onPressed: () {
+                        setState(() {});
+                      },
+                      child: Text('Button-sign-in'.i18n())),
                 )
               ],
             ),
